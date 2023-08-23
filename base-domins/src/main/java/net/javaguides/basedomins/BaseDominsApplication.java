5a765688bab39ad015c148fcb1bb4e2ca474a905
mohamed <mohamedsayedahmd@gmail.com>
2023-08-23 13:01:24 +0300

    first commit

---
+++ base-domins/src/main/java/net/javaguides/basedomins/BaseDominsApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.basedomins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDominsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDominsApplication.class, args);
+	}
+
+}
---
+++ base-domins/src/test/java/net/javaguides/basedomins/BaseDominsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.basedomins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDominsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
